8fe603dc7db50be0416df0934a8e0b9861691400
aleherrerahg <125672738+aleherrerahg@users.noreply.github.com>
2023-05-19 12:38:17 -0600

    Update EvPractica.js

--- EvPractica.js
+++ EvPractica.js
@@ -3,8 +3,8 @@ function Promo()
     let x=document.getElementById("fecha").value;
     var mes = x.substring(5, 7);
     let texto="";
-    con=(mes);
-    if(con==05)
+    con=(mes)*1;
+    if(con==5)
     {
         document.getElementById("comentario").innerHTML="Felicidades, obtuviste un 2*1";
         texto+="<img src='promocion.webp'>"
