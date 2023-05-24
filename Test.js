0747f1d6563bc6d9d0d1ec3097d0ee4881e8f317
aleherrerahg <125672738+aleherrerahg@users.noreply.github.com>
2023-05-24 08:15:06 -0600

    Update Test.js

--- Test.js
+++ Test.js
@@ -25,3 +25,27 @@ function califica()
         if(total<=15)//si su puntuaciÃ³n es menor o igual a 15, se le dirÃ¡ al usuario que necesita estudiar mÃ¡s
         document.getElementById("comentario").innerHTML="Estudia un poco m&#225s";
 }
+function generar()
+{
+    //Crear objeto o instancia en js
+    var doc = new jsPDF();
+
+    //Tamaño de letra
+    doc.setFontSize(40);
+    //Color del texto
+    doc.setTextColor(9,7,121);
+    doc.Text(40, 20, "Encuesta");
+    doc.setTextColor(17,122,209);
+    doc.setFontSize(30);
+    doc.text(50,35, "Es importante tu participación");
+    doc.setFontSize(20);
+    doc.text(20,50, "25 puntos. Excelente. \r Todas tus respuestas son correctas");
+    doc.text(20,50, "20 puntos. Muy bien. \r Obtuviste 4 respuestas correctas");
+    doc.text(20,50, "15 puntos. Regular. \r Obtuviste 3 respuestas correctas");
+    doc.text(20,50, "10 puntos. Mal \r Obtuviste 2 respuestas correctas");
+    doc.text(20,50, "5 puntos. Muy mal. \r Obtuviste 1 respuesta correcta");
+    doc.text(20,50, "0 puntos. Terrible. \r Todas tus respuestas son incorrectas");
+
+    var string = doc.output('datauristring');
+    $('iframe').attr('src', string);
+}
